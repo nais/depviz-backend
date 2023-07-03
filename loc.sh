4f443b6ca8b1bb29e83938b114071dba398a08f7
Audun Fauchald Strand <audun.fauchald.strand@nav.no>
2023-07-03 10:56:20 +0200

    continue from where we left off with loc.sh

--- loc.sh
+++ loc.sh
@@ -4,11 +4,16 @@ function getRepos() {
     gh api \
       -H "Accept: application/vnd.github+json" \
       -H "X-GitHub-Api-Version: 2022-11-28" \
-      /orgs/nais/repos --paginate | jq '.[].full_name' | tr -d '"' > repos.txt
+      /orgs/navikt/repos --paginate | jq '.[].full_name' | tr -d '"' > repos.txt
+}
+
+function cleanRepoFile(){
+  cat loc.txt |  awk '{print $1;}' >loc2.txt
+  comm -23 <(sort repos.txt) <(sort loc2.txt) > repos_clean.txt
 }
 
 function clone() {
-  Lines=$(cat repos.txt)
+  Lines=$(cat repos_clean.txt)
   for Repo in $Lines
   do
     gh repo clone $Repo tmprepo -- --depth 1
@@ -20,3 +25,7 @@ function clone() {
     printf "('$Repo is deleted automatically)\n\n\n"
   done
 }
+
+getRepos
+cleanRepoFile
+clone
