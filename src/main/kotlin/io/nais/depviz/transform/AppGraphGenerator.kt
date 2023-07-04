0d2a1c3d55c568017924cabccc172d8b2d37857a
Audun Fauchald Strand <audun.fauchald.strand@nav.no>
2023-07-04 10:28:33 +0200

    debugging is the best part of saas again

--- src/main/kotlin/io/nais/depviz/transform/AppGraphGenerator.kt
+++ src/main/kotlin/io/nais/depviz/transform/AppGraphGenerator.kt
@@ -1,6 +1,7 @@
 package io.nais.depviz.transform
 
 import io.nais.depviz.bigquery.ApplicationDependency
+import io.nais.depviz.model.external.Graph
 import io.nais.depviz.model.internal.InternalGraph
 import org.slf4j.LoggerFactory
 
@@ -13,9 +14,15 @@ class AppGraphGenerator(val applicationDependencies: List<ApplicationDependency>
     fun byEdgeCount() =
         internalGraph.toSizedGraph(internalGraph.edges.groupingBy { edge -> edge.toKey }.eachCount())
 
-    fun byLOC(locMap: Map<String, Int>) = internalGraph.toSizedGraph(applicationDependencies.associate {
-        it.key to locMap.getOrDefault(it.repo, 1)
-    })
+    fun byLOC(locMap: Map<String, Int>): Graph {
+        LOGGER.info("locmap " + locMap.entries.take(5).map { "${it.key} to ${it.value}" }.joinToString("::"))
+        val sizes = applicationDependencies.associate {
+            it.key to locMap.getOrDefault(it.repo, 1)
+        }
+        LOGGER.info("sizes " + sizes.entries.take(5).map { "${it.key} to ${it.value}" }.joinToString("::"))
+
+        return internalGraph.toSizedGraph(sizes)
+    }
 
      fun readFile() = MapFromResourcesFile().readAndParseWithDelimiter()
 }
