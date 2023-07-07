a508b38f8fb14a4a794471606419d0b861e072c5
Audun Fauchald Strand <audun.fauchald.strand@nav.no>
2023-07-07 12:10:34 +0200

    us scc and more logging. New version of nais.data

--- loc.sh
+++ loc.sh
@@ -5,6 +5,8 @@ function getRepos() {
     -H "Accept: application/vnd.github+json" \
     -H "X-GitHub-Api-Version: 2022-11-28" \
     /orgs/navikt/repos --paginate | jq '.[].full_name' | tr -d '"' >repos.txt
+  no_of_repos=$(cat repos.txt | wc -l)
+  echo "$no_of_repos repos in org"
 }
 
 function cleanRepoFile() {
@@ -15,13 +17,16 @@ function cleanRepoFile() {
 function clone() {
   Lines=$(cat repos_clean.txt)
   for Repo in $Lines; do
-    gh repo clone $Repo tmprepo -- --depth 1
+    gh repo clone $Repo tmprepo  -- --depth 1
     cd tmprepo
-    (echo -n "$Repo " &&
-      git ls-files | xargs wc -l | tail -1 | awk '{print $1;}') && echo >>../loc.txt
+    loc=$(scc -i bash,go,groovy,java,js,jsp,ipynb,kt,pl,py,rb,sh,sql,tf,ts,vue,rs.jsx -f json --no-size --no-cocomo -d | jq '.[].Lines' |  awk '{ sum += $1 } END { print sum }')
+    echo  "$Repo $loc"
+    (echo  "$Repo $loc")>>../loc.txt
     cd ..
     rm -rf tmprepo
-    printf "('$Repo is deleted automatically)\n\n\n"
+    no_of_repos=$(cat repos.txt | wc -l)
+    repos_done=$(cat loc.txt | wc -l)
+    echo "('$repos_done / $no_of_repos - $Repo is deleted automatically)\n\n\n"
   done
 }
 
